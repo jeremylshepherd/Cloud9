ecae5e9b91d2af854417ac8722efdb5789153a70
jeremylshepherd <jeremylshepherd@gmail.com>
2015-06-28 17:28:17 +0000

    Defining method for tests

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe "Creating todo lists" do
+    
+    def create_todo_list(options={})
+        options[:title] ||= "My todo list"
+        options[:description] ||= "This is my todo list."
+        
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+        
+        fill_in "Title", with: "My todo list"
+        fill_in "Description", with: "This is what I'm doing today."
+        click_button "Create Todo list"
+        
+        expect(page).to have_content("My todo list")
+    end
+    
     it "redirects to the todo list index page on success" do
         visit "/todo_lists"
         click_link "New Todo list"
@@ -47,5 +63,23 @@ describe "Creating todo lists" do
         
         visit "/todo_lists"
         expect(page).to_not have_content("This is what I'm doing today.")
+    end
+    
+    it "displays an error when the todo list has no description" do
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+        
+        fill_in "Title", with: "Grocery list"
+        fill_in "Description", with: ""
+        click_button "Create Todo list"
+        
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        expect(page).to_not have_content("Grocery list")
     end
 end
